fd1cde1f941c77e66fdfde6b1efe4e87d1c0a9e4
hujuu <1961946+hujuu@users.noreply.github.com>
2023-02-22 23:27:53 +0900

    Add private endpoint

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ Python FastAPI Auth0 integration example
 from typing import Optional
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, Response, Depends, status
 from fastapi.security import HTTPBearer
+from auth import VerifyToken
 
 middleware = [
     Middleware(
@@ -18,7 +19,6 @@ middleware = [
 
 origins = ["*"]
 
-# Scheme for the Authorization header
 token_auth_scheme = HTTPBearer()
 app = FastAPI(middleware=middleware)
 
@@ -46,3 +46,37 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.get("/headers")
 async def read_headers(user_agent: str | None = Header(None)):
     return {"User-Agent": user_agent}
+
+
+@app.get("/api/public")
+def public():
+    """No access token required to access this route"""
+
+    result = {
+        "status": "success",
+        "msg": ("Hello from a public endpoint! You don't need to be "
+                "authenticated to see this.")
+    }
+    return result
+
+
+@app.get("/api/header")
+def private(token: str = Depends(token_auth_scheme)):
+    """A valid access token is required to access this route"""
+
+    result = token.credentials
+
+    return result
+
+
+@app.get("/api/private")
+def private(response: Response, token: str = Depends(token_auth_scheme)):
+    """A valid access token is required to access this route"""
+
+    result = VerifyToken(token.credentials).verify()
+
+    if result.get("status"):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return result
+
+    return result
